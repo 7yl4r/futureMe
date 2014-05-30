eacc6af08641b3ed3fc1506efc4f4fc47890ca28
Tylar Murray <murray.tylar@gmail.com>
2014-05-30 11:40:50 -0400

    added user data loading from file

--- main.py
+++ main.py
@@ -3,22 +3,38 @@ This is the main script which runs all subscripts.
 '''
 
 from os import listdir, getcwd
+from sys import argv
+import imp
 
-# global consts
-MH    = 'python C:\Users\\twm4061\Documents\duststorm01-makehuman-commandline-d0aef702b167\makehuman\makehuman.py'  # path to makehuman .exe or .py main on the system
-BLEND = 'blender'  # path to blender on this system
+# === global consts ===
+# path to makehuman .exe or .py main on the system
+#MH    = 'python C:\Users\\twm4061\Documents\duststorm01-makehuman-commandline-d0aef702b167\makehuman\makehuman.py'
+MH = 'python C:\Users\\7yl4r\Documents\makehuman-commandline\makehuman\makehuman.py'
 
-# global vars
+# path to blender on this system
+BLEND = 'blender'
+
+# === global vars ===
 mh_options = {}  # dict of options to pass to makehuman
 scene_script = 'basic'  # path to the script to use in blender
 
-# get images, head model, user data
+
+
+# get images, head model
 IMAGES = [getcwd()+'/data/img/'+str(f) for f in listdir('./data/img/')]
 HEAD_SCAN= getcwd()+'/data/3dscan/mesh.obj'  # path to the head scan model
 
-# process images to get features
+# get base user data for model
+if len(argv) > 1:  # get user data from given file
+    print 'loading user data from ', argv[1]
+    USER_DATA = imp.load_source('module.name', argv[1])
+else:
+    print 'no user data file given'
+    raise NotImplementedError('must specify user data file')
+    
+# process images to get features (user features are selected user data (plus extracted features) used to generate the model)
 import image_processor.mock  # TODO: use real image processor
-user_features = image_processor.mock.getFeatures(images=IMAGES, headScan=HEAD_SCAN)
+user_features = image_processor.mock.getFeatures(images=IMAGES, headScan=HEAD_SCAN, userData=USER_DATA)
 
 # create model
 from modeler.make_human import makeModel
@@ -29,7 +45,7 @@ makeModel(user_features, MH, CURRENT_BODY)
 # TODO: compute changes to model conditioned on user data
 # (converts features+data+time into future features)
 # import predictor.mock.predictFeatures as predictFeatures
-future_features = user_features # predictFeatures(user_features, data)
+future_features = user_features # predictFeatures(user_features, USER_DATA)
 
 
 # TODO: modify body model
